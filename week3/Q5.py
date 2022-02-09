0cc0d6dc683c4826b414683d42f18f4335e31dbc
OYJ-hansung <dhdudwn95@hansung.ac.kr>
2022-02-09 16:54:17 +0900

    updates

--- week3/Q5.py
+++ week3/Q5.py
@@ -14,13 +14,13 @@ learning_rate = 0.01		# 학습률
 for i in range(1000):
 	pred = beta_gd * x_train + bias 						# 예측값 wx+b
 	error = sum(np.square(pred - y_train)) / len(y_train)	# MSE mean
+	#예측값과 실제 값의 차이
 
-	gd_w = sum(np.multiply(pred - y_train, 2 * x_train)) / len(y_train)	# MSE 미분값
-	gd_b = sum(np.multiply(pred - y_train, 2)) / len(y_train)			# MSE 미분값
+	gd_w = sum(np.multiply(pred - y_train, 2 * x_train)) / len(y_train)	# MSE 미분값 / gradient Descent Weight
+	gd_b = sum(np.multiply(pred - y_train, 2)) / len(y_train)			# MSE 미분값 / gradient Descent Bias
 	
 	beta_gd -= learning_rate * gd_w	# 기울기 조정
 	bias -= learning_rate * gd_b	# 절편 조정
 	
 	if i % 100 == 0: # 100번마다 출력
-		print(f'Epoch ({i:10d}/1000) cost: {error:10f}, w: {beta_gd.item(0):10f}\
-			,b: {bias.item(0):10f}')
+		print(f'Epoch ({i:10d}/1000) cost: {error:10f}, w: {beta_gd.item(0):10f}\,b: {bias.item(0):10f}')
---
+++ week3/Q5_YJ.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+xy = np.array([[1, 2, 3, 4, 5, 6], [3, 6, 9, 12, 15, 18]], float)
